e523f16321171c2c2ea3e63b22ca3efec6c5a16f
seanchuatech <seanchuatech@gmail.com>
2024-04-22 11:13:11 +0800

    Fixed overlapping navigation bar

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,8 +16,6 @@ const About = () => {
                   className="https://giphy.com/embed/qgQUggAC3Pfv687qPC"
                   width={480}
                   height={360}
-                  frameBorder={0}
-                  className="giphy-embed"
                   allowFullScreen=""
                 />
             </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = ({ heroRef, aboutRef, skillsRef, contactRef, projectRef, handleSc
   };
 
   return (
-    <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+    <nav className="bg-white dark:bg-gray-900 fixed w-screen z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Sean.</span>
