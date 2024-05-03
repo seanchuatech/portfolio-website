246a9a6752c1a31904138da1e66231a59cc45efe
seanchuatech <seanchuatech@gmail.com>
2024-05-03 10:48:24 +0800

    Added improved authentication with JTW

--- src/pages/user-management/Users.jsx
+++ src/pages/user-management/Users.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const Users = () => {
-  return (
-    <div>Users</div>
-  )
-}
+  const [users, setUsers] = useState();
+  const axiosPrivate = useAxiosPrivate();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-export default Users
+  useEffect(() => {
+    let isMounted = true;
+    const controller = new AbortController();
+
+    const getUsers = async () => {
+      try {
+          const response = await axiosPrivate.get('/users', {
+            signal: controller.signal
+          });
+          console.log(response.data);
+          isMounted && setUsers(response.data);
+      } catch (err) {
+          console.error(err);
+          navigate('/login', { state: { from: location }, replace: true });
+      }
+    }
+
+    getUsers();
+
+    return () => {
+      isMounted = false;
+      setTimeout(() => {
+        controller.abort();
+      }, 500); // Delay abort slightly
+    }
+  }, []);
+
+  return (
+    <article>
+      <h2>Users List</h2>
+      {users?.length
+        ? (
+            <ul>
+              {users.map((user, i) => <li key={i}>{user?.username}</li>)}
+            </ul>
+        ) : <p>No users to display</p>
+      }
+    </article>
+  );
+}
+export default Users;
